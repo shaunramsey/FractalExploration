f40d28737dc878f0a20b40531e60af178196ba2d
Michelle <39387234+michellely98@users.noreply.github.com>
2018-05-18 10:02:56 -0400

    Update tent_map_bifurcation.py
    fixed some comments

--- bifurcationplots/tent_map_bifurcation.py
+++ bifurcationplots/tent_map_bifurcation.py
@@ -9,13 +9,13 @@ Created on Fri May 18 08:41:41 2018
 import matplotlib.pyplot as plt
 import numpy as np
 
-x = np.arange(.5, 1, .001) #seed
-hori_x_range = np.arange(.5, 1, .001) #starts from -.5001 to skip over 0
+x = .5  #seed 
+hori_x_range = np.arange(.5, 1, .001) #range of c-values to iterate over
 
 for i in range (1, 100): #throws away first 100 
     x = np.multiply(hori_x_range, (1 - (2 * abs(x- .5) ) ) ) 
 
-for j in range(1,100): #goes through next 100 interations, where the orbit has settled down by now
+for j in range(1,100): #goes through next 100 iterations, where the orbit has settled down by now
     x = np.multiply(hori_x_range, (1 - (2 * abs(x- .5) ) ) )    #x mapped onto x squared plus c
     plt.scatter(hori_x_range, x, s = 1) #plots the orbit
 
