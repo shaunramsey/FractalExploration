e6737d986364cfc85044d4079d03dccef31ae8d1
Michelle <39387234+michellely98@users.noreply.github.com>
2018-05-18 09:26:00 -0400

    ƒ: x → c (1 − 2 | x − ½ | )
    This is a bifurcation diagram for ƒ: x → c (1 − 2 | x − ½ | )

---
+++ bifurcationplots/tent_map_bifurcation.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 18 08:41:41 2018
+
+@author: Michelle
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.arange(.5, 1, .001) #seed
+hori_x_range = np.arange(.5, 1, .001) #starts from -.5001 to skip over 0
+
+for i in range (1, 100): #throws away first 100 
+    x = np.multiply(hori_x_range, (1 - (2 * abs(x- .5) ) ) ) 
+
+for j in range(1,100): #goes through next 100 interations, where the orbit has settled down by now
+    x = np.multiply(hori_x_range, (1 - (2 * abs(x- .5) ) ) )    #x mapped onto x squared plus c
+    plt.scatter(hori_x_range, x, s = 1) #plots the orbit
+
+plt.xlabel("c")
+plt.ylabel("orbits")
+plt.title("Bifurcation Diagram")
+plt.show()
