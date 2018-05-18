ed16235233a4eb960cff5132d8455f9b1d9186b0
Shaun Ramsey <sramsey2@washcoll.edu>
2018-05-18 10:21:05 -0400

    images and sinfunc addition
    Added images for the x^2 + c bifurcation and also added the image and code for the sin(pi x)+c bifurcation

--- bifurcationplots/x2pcbifurcation.py
+++ bifurcationplots/x2pcbifurcation.py
@@ -10,11 +10,17 @@ import matplotlib.pyplot as plt
 # get a bifurcation plot of x^2+c
 # start with x = whatever, maybe 0?
 
+def func(x, c):
+    return np.multiply(x,x) + c
+
+def sinfunc(x,c):
+    return np.sin(np.pi*x)+c
+
 lower_bound = -2
 upper_bound = .25
 step_size = .0001
-initial_iterations = 100
-final_iterations = 100
+initial_iterations = 500
+final_iterations = 20
 x_init = 0 # could change this to be random
 
 c = np.arange(lower_bound, upper_bound, step_size)
@@ -22,12 +28,15 @@ x = x_init
 
 # let things settle to a steady state
 for i in range(initial_iterations):
-    x = np.multiply(x, x) + c
+    x = func(x, c)
 
 for i in range(final_iterations):
-    x = np.multiply(x, x) + c 
+    x = func(x, c)
     plt.scatter(c, x, s=1)
 
+plt.xlabel("Parametrized Value")
+plt.ylabel("Orbiting values")
+plt.title("Bifurcation Diagram")
 plt.show()
     
     
