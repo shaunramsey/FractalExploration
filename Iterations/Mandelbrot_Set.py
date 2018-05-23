029c207ee6876dccad145f81a190c93bca508b92
Michelle <39387234+michellely98@users.noreply.github.com>
2018-05-23 11:42:49 -0400

    Julia and Mandelbrot
    -cleaner Julia Set code with correct (awesome) image
    -image has correct axis units and scale
    -Mandelbrot Set code and image

--- Iterations/Mandelbrot_Set.py
+++ Iterations/Mandelbrot_Set.py
@@ -23,18 +23,18 @@ Created on Mon May 21 15:06:43 2018
 @author: Michelle 
 """
 
+#The goal is to graph only the Julia Sets that are connected
+
 import matplotlib.pyplot as plt
 import numpy as np
 
-
-c = 0.4 - 0.325j #parameter
-#for a julia set, you want to change z (the initial value) but keep c the same 
+#for a mandelbrot set, you want to change c (the complex number) but keep z the same (the origin 0)
 
 
 high_num = 200 #above this we consider infinity
 neg_num = -200 #below this we consider infinity
-lowerbound = -1.2 #the lower bound of the values we consider on real and i
-upperbound = 1.2  #upper bound of values we consider on real and i
+lowerbound = -2 #the lower bound of the values we consider on real and i
+upperbound = 2  #upper bound of values we considero n real and i
 stepsize = 0.01 # increment size between upper and lower bounds
 steps = int((upperbound-lowerbound)/stepsize) # how many steps it'll take
 iters = 100 #how many iterations we'll take to try to find infinity
@@ -45,23 +45,26 @@ grid = grid.reshape((steps,steps))
 rp = lowerbound 
 ip = lowerbound
 
-#filling up our initial zpts array with every possible value of complex numbers
-for stepx in range (steps):
+#filling up our c array with every possible value of complex numbers
+for stepx in range(steps): #we want to get all the numbers from the lowerbound to the upperbound
     print("Step:",stepx,"/",steps)
-    ip = lowerbound 
+    ip = lowerbound
     for stepy in range(steps):
-        z = complex(rp, ip) #this is our initial z 
+        c = complex(rp, ip) #this is our initial c 
+        z = 0
         for i in range(iters):
             z = np.multiply(z,z) + c  #get the next z from this z 
             if z.real > high_num or z.imag > high_num or z.real < neg_num or z.imag < neg_num:
                 break
         grid[stepy][stepx] = i #the graph is colored according to how long it takes to get to infinity
-        ip += stepsize   
+            #by switching the stepy and stepx, you get a set thats oriented like online images -- real axis as x, imaginary as y
+        ip += stepsize       
     rp += stepsize
+   
     
-plt.suptitle("Julia Set z=z^2+c")
-plt.xlabel("Real units")
-plt.ylabel("Imaginary units")
+plt.suptitle("Mandelbrot Set")
+plt.xlabel("real")
+plt.ylabel("imaginary")
 plt.title("c="+str(c))
-plt.imshow(grid,cmap="terrain", origin = 'lower', extent=[lowerbound,upperbound,lowerbound,upperbound]) #origin = lower sets the 0,0 point on the axis to the bottom left
+plt.imshow(grid,cmap="terrain", origin = 'lower')
 plt.show()
