131812c88e31d962fb14a09408ce376cc0f0cad1
Rachel <38047182+r-chambers@users.noreply.github.com>
2018-07-10 08:48:56 -0400

    Add log checks to lyapunov_fractal_logistic_map.py
    - Added checks in the F prime function to handle answers of 0 or inf (which log obviously cannot handle)

--- Fractals/Markus-Lyapunov Fractals/lyapunov_fractal_logistic_map.py
+++ Fractals/Markus-Lyapunov Fractals/lyapunov_fractal_logistic_map.py
@@ -5,16 +5,16 @@ from timeit import default_timer as timer
 start = timer()
 
 # PARAMETERS TO CHANGE THE FRACTAL GENERATED
-seq = "BA"                     # sequence to alternate r values
+seq = "AB"                   # sequence to alternate r values
 a_lb = 2                       # b1 lower bound
 a_ub = 4                       # b1 upper bound
 b_lb = 2                       # b2 lower bound
 b_ub = 4                       # b2 upper bound
 
 # PARAMETERS REFINING ACCURACY OF FRACTAL PICTURE GENERATED
-num_warmups = 1200             # number of "warmups" or throwaway iterations before computing lyapunov exponent
-num_lyap_iterations = 120      # number of iterations used to compute the lyapunov exp
-steps = 500                    # steps between b1 and b2 values on axes -- higher it is, the better the picture
+num_warmups = 10             # number of "warmups" or throwaway iterations before computing lyapunov exponent
+num_lyap_iterations = 100      # number of iterations used to compute the lyapunov exp
+steps = 500                   # steps between b1 and b2 values on axes -- higher it is, the better the picture
 
 # LOGISTIC MAP THAT GIVES US THE NEXT X
 @jit
@@ -24,7 +24,11 @@ def F(x, curr_r):
 # DERIVATIVE OF F -- USED TO COMPUTE THE LYAPUNOV EXPONENT
 @jit
 def Fprime(x, curr_r):
-     return curr_r * (1 - (2 * x))
+    ans = curr_r * (1 - (2 * x))
+    ans[ans == 0] = 0.0001
+    ans[ans == -np.inf] = -1000
+    ans[ans == np.inf] = 1000
+    return ans
  
 # RETURNS THE CORRECT B-VALUE BASED ON THE CURRENT ITERATION
 @jit
@@ -41,7 +45,7 @@ def getlyapexponent(time_scale):
     b1, b2 = time_scale
     
     x = .5          # initial value of x
-    lyapsum = 0     # initializing lyapunov sum for use later
+    lyap_sum = 0     # initializing lyapunov sum for use later
     
     # do warmups, to discard the early values of the iteration to allow the orbit to settle down
     for i in range(num_warmups):
@@ -49,31 +53,36 @@ def getlyapexponent(time_scale):
         
     
     for i in range(num_warmups, num_lyap_iterations + num_warmups):
-        lyapsum += np.log( np.abs(Fprime(x, getseqval(i, b1, b2) ) ) )
+        #print("lysum", lyapsum, "i", i)
+        
+        lyap_sum += np.log( np.abs(Fprime(x, getseqval(i, b1, b2) ) ) )
+        
         # get next x
         x = F(x, getseqval(i, b1, b2))
+        
+        
     
-    return (lyapsum / num_lyap_iterations)
+    return (lyap_sum / num_lyap_iterations)
 
 # CREATING FRACTAL IMAGE 
 a = np.linspace(a_lb, a_ub, steps)   #range of b1 values
 b = np.linspace(b_lb, b_ub, steps)   #range of b2 values
 aa, bb = np.meshgrid(a, b)
             
-fractal_grid = []                                # "grid" containing lyapunov exponents for each point in points
-fractal_grid.append(getlyapexponent( (bb, aa) )) 
-fractal_grid = np.reshape(fractal_grid, (steps, steps)) # fractal_grid is then reshaped into a grid
+fractal_grid = getlyapexponent( (bb, aa) )
 
-lyap_cmap = plt.get_cmap('nipy_spectral')         # creating our own colormap to use "set_over" with    
-lyap_cmap.set_over('black')              # any value over vmax is colored black
-
-plt.figure()
+# CREATING AND ADJUSTING GRAPH
+plt.figure()       # creating new window for each graph that is run
+lyap_cmap = plt.get_cmap('nipy_spectral')   # creating our own colormap to use "set_over" with    
+lyap_cmap.set_over('black')                 # any value over vmax is colored black
+lyap_cmap.set_under('#5e1d77')              # any value under vmin is colored dark purple
 plt.suptitle("Lyanpuov fractal for logistic map")
-plt.title("string = " + seq)
+plt.title("Seq: " + seq)
 plt.xlabel("a")
 plt.ylabel("b")
-plt.imshow(fractal_grid, cmap = lyap_cmap, vmax = 0, origin = "lower", extent = (a_lb, a_ub, b_lb, b_ub))
+
+im = plt.imshow(fractal_grid, cmap = lyap_cmap, vmin = -2, vmax = 0, origin = "lower", extent = (a_lb, a_ub, b_lb, b_ub))
+plt.colorbar(im)
 
 end = timer()
-
-print("elapsed time for meshgrid: " + str(end - start))
+print("elapsed time: " + str(end - start))
